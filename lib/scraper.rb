ec1a3256cbdaf9660975807cad8fcf20342d521b
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-12-28 11:35:40 -0500

    commit tree 19a8c8027b5229e612e50f333462f590885c2457

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,24 @@
 require 'open-uri'
-require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+
+    nested_list = Nokogiri::HTML(html)
+
+    students = []
+
+    nested_list.css("div.student-card").each do |student|
+      students << {
+        :name => student.css("h4.student-name").text,
+         :location => student.css("p.student-location").text,
+         :profile_url => student.children[1].attributes["href"].value
+      }
+    end
+    # binding.pry
+    students
   end
 
   def self.scrape_profile_page(profile_url)
